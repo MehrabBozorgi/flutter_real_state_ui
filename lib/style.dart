41fb284bab2e44bba35894279e387e54087d8cc4
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-04-30 18:49:50 +0430

    flutter_real_state_ui

---
+++ lib/style.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF132F45);
+const backGroundColor = Color(0xFFFFFFFF);
+const iconColor = Color(0xFFCCC7A3);
+
+const iconTitleStyle = TextStyle(
+  color: Colors.grey,
+  fontSize: 14,
+);
+const primaryTitleStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+const primaryBigTitleStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 22,
+  fontWeight: FontWeight.bold,
+);
+const whitTitleStyle = TextStyle(
+  color: backGroundColor,
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+const desc1WhiteStyle = TextStyle(
+  color: backGroundColor,
+  fontSize: 14,
+);
+const desc2WhiteStyle = TextStyle(
+  color: backGroundColor,
+  fontSize: 12,
+);
+const descBlackWhiteStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 12,
+);
+
+const primarySmallTitleStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
